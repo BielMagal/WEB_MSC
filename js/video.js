5db5f186b36e5a1ee9ad93176cfa411c010ce1b4
Biel Magal <bielpires1995@gmail.com>
2018-05-03 14:31:41 -0300

    Widget-Video

---
+++ js/video.js
@@ -0,0 +1,9 @@
+function changeVideo(){
+  let link = document.getElementById("linkVideo").value//"https://youtu.be/xKX125srwYU";
+  link = link.split("v=");
+  link = link[1];
+  if(link.length > 0){
+    link = "https://www.youtube.com/embed/" + link;
+    document.getElementById("frameVideo").src = link;
+  }
+}
