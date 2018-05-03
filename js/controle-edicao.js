3b0a04adbc508c58a1a5a4cd8cfe76f4123cf097
Vinícius <viniciusvmda@gmail.com>
2018-05-03 02:42:14 -0300

    Controle de visualização do header finalizado

---
+++ js/controle-edicao.js
@@ -0,0 +1,24 @@
+const CLASS_INVISIVEL = 'invisivel';
+
+function habilitaCamposEdicao() {
+}
+
+function habilitaCamposVisualizacao() {
+}
+
+
+function alteraVisualizacao() {
+  let ctrlVisualizacao = document.querySelector('#controle-visualizacao');
+  let ctrlEdicao = document.querySelector('#controle-edicao');
+  if (ctrlVisualizacao.classList.contains(CLASS_INVISIVEL)) {
+    ctrlVisualizacao.classList.remove(CLASS_INVISIVEL);
+    ctrlEdicao.classList.add(CLASS_INVISIVEL);
+    habilitaCamposVisualizacao();
+  } else {
+    ctrlEdicao.classList.remove(CLASS_INVISIVEL);
+    ctrlVisualizacao.classList.add(CLASS_INVISIVEL);
+    habilitaCamposEdicao();
+  }
+}
+
+export { alteraVisualizacao };
--- js/script-mundo.js
+++ js/script-mundo.js
@@ -1,5 +1,12 @@
 import {keyEvent} from './controle-personagem.js';
+import {alteraVisualizacao} from './controle-edicao.js';
 
-// Pega o body
+// Adiciona listener para entrada do teclado
 let bodyEl = document.querySelector('body');
 bodyEl.addEventListener('keydown', keyEvent);
+
+// Adicionar listener para controlar botões visíveis na tela
+let botoesControle = document.querySelectorAll('.btn-controle');
+for (let botao of botoesControle) {
+  botao.addEventListener('click', alteraVisualizacao);
+}
