f9ddfd21309aa4e554becf99c7152ad20698693d
Vinícius <viniciusvmda@gmail.com>
2018-05-03 04:06:59 -0300

    Adicionado grid aos cards

---
+++ js/controle-widgets.js
@@ -0,0 +1,13 @@
+const MODAL_ADD_WIDGET = ;
+
+// Abre o modal que adiciona widget
+function abreModalAddWidget() {
+}
+
+// Adiciona os widgets ao select do modal de adição de widget
+function populaSelect() {
+}
+
+// Habilita os widgets da tela selecionados
+function habilitaWidgets() {
+}
