4ae7ef792ba2d4cc9ff88aac0eb63aa90c889ac5
Vinícius <viniciusvmda@gmail.com>
2018-06-28 08:33:04 -0300

    Configurado banco remoto

--- config/banco.js
+++ config/banco.js
@@ -6,7 +6,11 @@ const bd_porta = process.env.MONGODB_PORT || '27017';
 const bd_nome = process.env.MONGODB_NAME || 'msc';
 const session_secret = process.env.SESSION_SECRET || 'keyboard cat';
 
+const urlLocal = 'mongodb://' + bd_usuario + ':' + bd_senha + '@' + bd_host + ':' + bd_porta + '/' + bd_nome;
+const urlRemota = 'mongodb://admin:admin2018@ds221271.mlab.com:21271/msc';
+
 module.exports = {
-  url : 'mongodb://' + bd_usuario + ':' + bd_senha + '@' + bd_host + ':' + bd_porta + '/' + bd_nome,
+  url: urlRemota,
+  // url : urlLocal
   session_secret: session_secret
 };
