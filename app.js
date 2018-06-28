1782015a991b19459483be43347ef55e20fb52a7
BielMagal <bielpires1995@gmail.com>
2018-06-28 09:13:54 -0300

    heroku config

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.use(function(req, res, next){
 });
 
 // Inicia servidor
-const server = app.listen(3000, () => {
+const server = app.listen(5000, () => {
   let host = server.address().address;
   const port = server.address().port;
   if (host.length == 2) {
